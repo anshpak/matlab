4ac7195bf9aa93e404c1485a7e0387c32a9f815a
Andrey Shpak <anshpak.by@gmail.com>
2022-12-29 00:54:59 +0300

    feat: add a square interpolation

---
+++ Shpak_Lb10/twin_7.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = twin_7(inputArg1,inputArg2)
+%TWIN_7 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
